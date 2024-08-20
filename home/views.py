94e45bc889d686499d6c902ca1a4300c28abae7e
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-20 02:02:24 -0600

    actualizar vistas de home

--- home/views.py
+++ home/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def Index(request):
-    return HttpResponse("Home")
+    return render(
+        request=request,
+        template_name="index.html"
+    )
