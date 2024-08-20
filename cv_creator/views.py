3e9cd79d335e1af4524ee83fd6a1f529c12b4cd5
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-20 02:01:49 -0600

    editar vistas de cv_creator

--- cv_creator/views.py
+++ cv_creator/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
-def create(request):
-    return HttpResponse("Create CV form")
+def create(request:HttpRequest):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        if True:
+            pass
+
+    else:
+        # form = CV_form()
+        return render(request, "create.html")
