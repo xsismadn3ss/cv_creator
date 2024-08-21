d95cba7973542d592e908a3f47c816626988c66e
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-21 03:23:20 -0600

    update home urls

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', view=views.Index, name='home'),
     path('accounts/login/', view=views.login_view, name='login'),
     path('accounts/signup/', view=views.signup, name='signup'),
-    path('accounts/logout/', view=views.signout, name='logout')
+    path('accounts/logout/', view=views.signout, name='logout'),
+    path('accounts/', view=views.account, name='account'),
+    path('accounts/edit', view=views.edit_account, name='edit_account')
 ]
