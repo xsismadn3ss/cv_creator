1d3d1d29023e41366316ff44dca0282de7d82f84
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-21 10:15:50 -0600

    update urls

--- cv_creator/urls.py
+++ cv_creator/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path(route='', view=views.dashboard, name="dashboard"),
     path(route='create', view=views.login_required(views.create), name='create'),
     path(route='result/<int:id>', view=views.result, name="result"),
+    path(route='edit/<int:id>', view=views.edit, name="edit")
 ]
