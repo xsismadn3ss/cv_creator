6a6f8a7d190d5e2a11c0ef7f496144ba28a5f9a6
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-21 10:16:03 -0600

    add edit cv view

--- cv_creator/views.py
+++ cv_creator/views.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpRequest, HttpResponse
 from django.contrib.auth.decorators import login_required
@@ -8,10 +9,12 @@ from django.urls import reverse
 
 @login_required()
 def create(request: HttpRequest):
+    
     if request.method == "POST":
 
         user = User.objects.get(id=request.user.id)
 
+        title = request.POST.get("title")
         name = f"{user.first_name} {user.last_name}"
         email = user.email
         phone = request.POST.get("phone")
@@ -25,6 +28,7 @@ def create(request: HttpRequest):
         lang = request.POST.get("lang")
 
         new_cv = CV(
+            title = title,
             name=name,
             email=email,
             phone=phone,
@@ -47,6 +51,28 @@ def create(request: HttpRequest):
         return render(request, "create.html")
 
 
+def edit(request:HttpRequest, id):
+    template = "edit.html"
+    cv = get_object_or_404(CV, id=id)
+
+    if request.method == "POST":
+        cv.title = request.POST.get("title")
+        cv.phone = request.POST.get("phone")
+        cv.urls = request.POST.get("urls")
+        cv.job = request.POST.get("job")
+        cv.about = request.POST.get("about")
+        cv.exp = request.POST.get("exp")
+        cv.edu = request.POST.get("education")
+        cv.top_skills = request.POST.get("top")
+        cv.soft_skills = request.POST.get("soft")
+        cv.lang = request.POST.get("lang")
+        cv.save()
+        
+        return redirect('dashboard')
+
+
+    return render(request, template, {"cv":cv})
+
 @login_required()
 def result(request: HttpRequest, id):
     cv = get_object_or_404(CV, id=id, user=request.user.id)
@@ -58,5 +84,12 @@ def result(request: HttpRequest, id):
     return render(request, "result.html", {"cv": cv, "links": links, "edu": edu, 'top':top, 'soft':soft, 'lang':lang})
 
 def dashboard(request:HttpRequest):
+
     cv_list = CV.objects.filter(user=request.user.id).values()
+
+    if request.method == "POST":
+        id = request.POST.get('id')
+        CV.objects.get(id=id).delete()
+        return render(request, 'dashboard.html', {'cvList':cv_list})
+
     return render(request, 'dashboard.html', {'cvList':cv_list})
