59f80134424507363f6d8a031b6776bb158e1436
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-20 23:10:47 -0600

    add login, logout and register url

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', view=views.Index, name='home')
+    path('', view=views.Index, name='home'),
+    path('accounts/login/', view=views.login_view, name='login'),
+    path('accounts/signup/', view=views.signup, name='signup'),
+    path('accounts/logout/', view=views.signout, name='logout')
 ]
