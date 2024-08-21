66e54e0b5b1c1657bce142ab73b67fcaacb885dd
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-21 03:23:54 -0600

    rename route

--- web/urls.py
+++ web/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')), #home app
-    path('creator', include('cv_creator.urls')) #creator app
+    path('cv/', include('cv_creator.urls')) #creator app
 ]
