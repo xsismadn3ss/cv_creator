e1dbdad56a3a3f76dc1e99d8574aadadc2328750
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-21 03:20:15 -0600

    add result route

--- cv_creator/urls.py
+++ cv_creator/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(route='', view=views.login_required(views.create), name='create'),
-    path(route='result/', view=views.result, name="result")
+    path(route='', view=views.dashboard, name="dashboard"),
+    path(route='create', view=views.login_required(views.create), name='create'),
+    path(route='result/<int:id>', view=views.result, name="result"),
 ]
