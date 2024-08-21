fedba811dce080788e1c7a6a9d615a6897da0a95
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-20 23:10:18 -0600

    add login, logout and register view

--- home/views.py
+++ home/views.py
@@ -1,10 +1,77 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponsePermanentRedirect
+from django.shortcuts import redirect, render
+from .forms import SignupForm, LoginForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 
-def Index(request):
-    return render(
-        request=request,
-        template_name="index.html"
-    )
+
+def Index(request:HttpRequest):
+    template = "index.html"
+    if request.user.is_authenticated == True:
+        print("autenticado")
+        return render(request, template, {'is_auth': True})
+
+    else:
+        print("no autenticado")
+        return render(request, template, {"is_auth": False})
+
+
+def login_view(request:HttpRequest):
+    template = "login.html"
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+
+            user = authenticate(request, username=username, password=password)
+            print(user)
+            if user is not None:
+                login(request=request, user=user, backend=None)
+                return redirect('create')
+                # return HttpResponsePermanentRedirect('/create')
+
+            else:
+                context = {
+                    "form": LoginForm(),
+                    "warning": "Datos incorrectos",
+                    "is_auth": True,
+                }
+                return render(request, template, context)
+
+    else:
+        form = LoginForm()
+        return render(request, template, {"form": form, "warning": ""})
+
+
+def signup(request:HttpRequest):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            confirm = form.cleaned_data["confirm"]
+
+            if password == confirm:
+                new_user = User(username=username, password=make_password(password))
+                new_user.save()
+                return redirect('home')
+                # return HttpResponsePermanentRedirect('/')
+    else:
+        form = SignupForm
+        return render(request, "signup.html", {"form": form})
+
+
+def signout(request:HttpRequest):
+    if request.method == "POST":
+
+        logout(request)
+        return redirect('home')
+        # return HttpResponsePermanentRedirect('/')
+    
+    else:
+        return render(request, 'logout.html')
