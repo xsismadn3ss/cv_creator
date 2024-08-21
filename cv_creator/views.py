70874207f278e21c21e6d85f43587de0cda8395f
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-21 03:20:33 -0600

    edit create view

--- cv_creator/views.py
+++ cv_creator/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpRequest, HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .models import CV
+from django.urls import reverse
 
 
 @login_required()
-def create(request:HttpRequest):
+def create(request: HttpRequest):
     if request.method == "POST":
 
-        name = request.POST.get("name")
-        email = request.POST.get("email")
+        user = User.objects.get(id=request.user.id)
+
+        name = f"{user.first_name} {user.last_name}"
+        email = user.email
         phone = request.POST.get("phone")
         urls = request.POST.get("urls")
         job = request.POST.get("job")
@@ -20,14 +24,39 @@ def create(request:HttpRequest):
         soft = request.POST.get("soft")
         lang = request.POST.get("lang")
 
-        new_cv = CV(name=name, email=email, phone=phone, urls = urls, job = job, about = about, exp = exp, edu= education, top_skill = top, soft_skills = soft, lang = lang)
+        new_cv = CV(
+            name=name,
+            email=email,
+            phone=phone,
+            urls=urls,
+            job=job,
+            about=about,
+            exp=exp,
+            edu=education,
+            top_skills=top,
+            soft_skills=soft,
+            lang=lang,
+            user=request.user,
+        )
+        new_cv.save()
 
-        return HttpResponse("Datos enviados con exito!")
-        # return HttpResponseRedirect('create/add_exp')
+        cv = CV.objects.last()
+        return redirect(reverse("result", args=[cv.id]))
 
     else:
         return render(request, "create.html")
 
 
-def result(request:HttpResponse):
-    return HttpResponse("Datos enviados con exito")
+@login_required()
+def result(request: HttpRequest, id):
+    cv = get_object_or_404(CV, id=id, user=request.user.id)
+    links = cv.urls.split(",")
+    edu = cv.edu.split(",")
+    top = cv.top_skills.split(",")
+    soft = cv.soft_skills.split(",")
+    lang = cv.lang.split(",")
+    return render(request, "result.html", {"cv": cv, "links": links, "edu": edu, 'top':top, 'soft':soft, 'lang':lang})
+
+def dashboard(request:HttpRequest):
+    cv_list = CV.objects.filter(user=request.user.id).values()
+    return render(request, 'dashboard.html', {'cvList':cv_list})
