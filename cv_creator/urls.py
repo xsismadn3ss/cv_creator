a84d20252f44369283d227592f4ddae88b8032fc
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-20 23:07:39 -0600

    update cv creator urls

--- cv_creator/urls.py
+++ cv_creator/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(route='', view=views.create, name='create')
+    path(route='', view=views.login_required(views.create), name='create'),
+    path(route='result/', view=views.result, name="result")
 ]
