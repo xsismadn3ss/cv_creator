69ff4b5a55a420aad20c158893e39450e3652ac0
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-21 03:23:34 -0600

    update home urls

--- home/views.py
+++ home/views.py
@@ -1,39 +1,37 @@
-from django.http import HttpRequest, HttpResponse, HttpResponsePermanentRedirect
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
-from .forms import SignupForm, LoginForm
+from .forms import SignupForm, LoginForm, AccountForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 
-# Create your views here.
 
-
-def Index(request:HttpRequest):
+def Index(request: HttpRequest):
     template = "index.html"
     if request.user.is_authenticated == True:
         print("autenticado")
-        return render(request, template, {'is_auth': True})
+        return render(request, template, {"is_auth": True})
 
     else:
         print("no autenticado")
         return render(request, template, {"is_auth": False})
 
 
-def login_view(request:HttpRequest):
+def login_view(request: HttpRequest):
     template = "login.html"
 
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
+            username = request.POST["username"]
+            password = request.POST["password"]
 
             user = authenticate(request, username=username, password=password)
             print(user)
             if user is not None:
                 login(request=request, user=user, backend=None)
-                return redirect('create')
-                # return HttpResponsePermanentRedirect('/create')
+                return redirect("create")
 
             else:
                 context = {
@@ -48,30 +46,59 @@ def login_view(request:HttpRequest):
         return render(request, template, {"form": form, "warning": ""})
 
 
-def signup(request:HttpRequest):
+@login_required()
+def account(request: HttpRequest):
+    template = "account.html"
+    user = User.objects.get(id=request.user.id)
+    return render(request, template, {"user": user})
+
+
+@login_required()
+def edit_account(request: HttpRequest):
+
+    template = "edit_account.html"
+    user = User.objects.get(id=request.user.id)
+
+    if request.method == "POST":
+        form = AccountForm(request.POST, instance=user)
+        if form.is_valid():
+            print("enviado")
+            form.save()
+            return redirect("account")
+        
+        else:
+            print("no valido")
+            return render(request, template, {'form': AccountForm()})
+
+    else:
+        form = AccountForm(instance=user)
+        return render(request, template, {"form": form})
+
+
+def signup(request: HttpRequest):
+
     if request.method == "POST":
         form = SignupForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
             confirm = form.cleaned_data["confirm"]
 
             if password == confirm:
                 new_user = User(username=username, password=make_password(password))
                 new_user.save()
-                return redirect('home')
-                # return HttpResponsePermanentRedirect('/')
+                return redirect("home")
     else:
-        form = SignupForm
+        form = SignupForm()
         return render(request, "signup.html", {"form": form})
 
 
-def signout(request:HttpRequest):
+@login_required()
+def signout(request: HttpRequest):
     if request.method == "POST":
 
         logout(request)
-        return redirect('home')
-        # return HttpResponsePermanentRedirect('/')
-    
+        return redirect("home")
+
     else:
-        return render(request, 'logout.html')
+        return render(request, "logout.html")
