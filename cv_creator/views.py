3c263927be8b89318ccfe52cfe35551a72665546
xs_ismand3ss <2477carlos.a@gmail.com>
2024-08-20 19:09:35 -0600

    caputar datos de formulario

--- cv_creator/views.py
+++ cv_creator/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from .functions import trim
 
 
 # Create your views here.
 def create(request:HttpRequest):
     if request.method == "POST":
+        # personal
         name = request.POST.get("name")
-        if True:
-            pass
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        urls = request.POST.get("urls")
+        # description
+        job = request.POST.get("job")
+        about = request.POST.get("about")
+        exp = request.POST.get("exp")
+        education = request.POST.get("education")
+        # skills
+        top = request.POST.get("top")
+        soft = request.POST.get("soft")
+        lang = request.POST.get("lang")
+
+        data = trim(name,email, phone, urls, job, about, exp, education, top, soft, lang)
+        print(data)
+
+        return HttpResponse("Datos recibidos correctamente")
 
     else:
-        # form = CV_form()
         return render(request, "create.html")
